38cc0c1fd023597d7228d3cece00c6c28c2f5422
David <vakilida@gmail.com>
2014-12-11 22:32:32 -0500

    Updated script for console statistics

--- hello.py
+++ hello.py
@@ -1,18 +1,8 @@
-statistic = "Avg time per move" + ": "
-sum = 0
 count = 0 
 
 if __name__ == "__main__":
     with open('Text.txt', 'rU') as f:
         for line in f:
-            if statistic in line:
-                number = float(line.rsplit(statistic)[1])
-                print number
-                sum += float(number)
-                count += 1
-
-        print "count is" + str(count)
-        print "average statistic is " + str(sum / count) 
  
 
 
